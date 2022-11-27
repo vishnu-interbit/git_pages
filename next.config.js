0192e5a341275ad00dcf0f6fd6399e82e70672fa
vishnu.interbit <vishnu.interbitsolutions.com>
2022-11-27 22:30:26 +0530

    test2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ["localhost", "towingminneapolis.us"] },
 };
 
 module.exports = nextConfig;
